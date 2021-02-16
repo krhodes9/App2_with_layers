7113a143699b696cbe79e65b9d92562e24963fb1
krhodes9 <61336730+krhodes9@users.noreply.github.com>
2021-02-16 10:24:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-require(["esri/Map", "esri/views/MapView"], function(Map, MapView) {
+require(["esri/Map", "esri/views/MapView", "esri/layers/FeatureLayer"], function(Map, MapView, FeatureLayer) {
         var map = new Map({
-          basemap: "dark-gray"
+          basemap: "hybrid"
         });
 
         var view = new MapView({
           container: "viewDiv",
           map: map,
-          zoom: 10,
-          center: [-90, 38] // longitude, latitude
+          center: [15.7832, 20.5085],
+          scale: 25000003
+ 
         });
+
+        var featureLayer = new FeatureLayer({
+          url: "https://services5.arcgis.com/FMvVojoqni7sergv/arcgis/rest/services/Locust_Distribution_Areas_in_Africa/FeatureServer"
+        });
+
+        map.add(featureLayer);
+   var featureLayer_2 = new FeatureLayer({
+  url: "https://services5.arcgis.com/FMvVojoqni7sergv/arcgis/rest/services/Locust_Breeding_Sites_in_Africa/FeatureServer"
+});
+
+map.add(featureLayer_2);
       });
